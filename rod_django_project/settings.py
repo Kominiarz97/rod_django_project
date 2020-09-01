e9c6504e08d5dab245690cf040db483ed980f457
Maciek <kominiarz1997@gmail.com>
2020-09-01 12:54:19 +0200

    connection with postgresql

--- rod_django_project/settings.py
+++ rod_django_project/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'rod_django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rod_django',
+        'USER':'postgres',
+        'PASSWORD':'root',
+        'HOST':'localhost'
     }
 }
 
