f1e08e06e085663ec8fa17bfab44017a2415b519
Maciek <kominiarz1997@gmail.com>
2020-10-27 22:28:24 +0100

    still progress

--- rod/views.py
+++ rod/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render,redirect
-from django.contrib.auth.models import  User,auth
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import  User, auth
+from .models import Zagrozenia, Zgloszenia
 
 def home(request):
     if request.user.is_authenticated:
@@ -9,20 +10,23 @@ def home(request):
 
 def last_reports(request):
     if request.user.is_authenticated:
-        return render(request,'rod/last_reports.html',{'title':'Ostatnie zgłoszenia'})
+        zgloszenia = Zgloszenia.objects.all()
+
+        return render(request, 'rod/last_reports.html', {'title' : 'Ostatnie zgłoszenia', 'zgloszenia':zgloszenia})
     else:
         return redirect('/')
 
 
 def drones(request):
     if request.user.is_authenticated:
-        return render(request,'rod/drones.html',{'title':'Drony'})
+        return render(request,'rod/drones.html', {'title':'Drony'})
     else:
         return redirect('/')
 
 def map(request):
     if request.user.is_authenticated:
-        return render(request,'rod/map.html',{'title':'Mapa'})
+        zgloszenia = Zgloszenia.objects.all()
+        return render(request,'rod/map.html',{'title':'Mapa', 'zgloszenia':zgloszenia})
     else:
         return redirect('/')
 
@@ -49,3 +53,6 @@ def noninterv_archive(request):
         return render(request,'rod/noninterv_archive.html',{'title':'Zgłoszenia wymagające interwencji'})
     else:
         return redirect('/')
+
+
+
