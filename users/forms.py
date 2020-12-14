df7bed8811bc017328c8b092a345d040fc17b473
kabzel55 <patrico2dv@gmail.com>
2020-12-14 19:46:37 +0100

    fixed profil email

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,13 @@
 from django import forms
 from django.contrib.auth.models import User
 
+
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
+
     class Meta:
         model = User
-        fields = ['first_name', 'last_name','username']
+        fields = ['first_name', 'last_name', 'username', 'email']
         labels = {
             'first_name': 'Imie',
             'last_name': 'Nazwisko',
