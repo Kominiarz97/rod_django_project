155e2a334a18debae13a7ce77be7a3f68ee374ab
kabzel55 <patrico2dv@gmail.com>
2020-12-18 21:07:13 +0100

    Clean up

--- rod/views.py
+++ rod/views.py
@@ -11,7 +11,14 @@ def home(request):
     else:
         return redirect('/')
 
+def map(request):
+    if request.user.is_authenticated:
+        zgloszenia = Zgloszenia.objects.all()
+        return render(request,'rod/map.html',{'title':'Mapa', 'zgloszenia':zgloszenia})
+    else:
+        return redirect('/')
 
+# -----------------------------------------------------------------------------ZGŁOSZENIA------------------------------------------------------------
 def last_reports(request):
     if request.user.is_authenticated:
         zgloszenia = Zgloszenia.objects.filter(zarejestrowane=False).order_by('-id_zgloszenia')
@@ -54,19 +61,7 @@ def report_ignore(request, pk):
 
     else:
         return redirect('/')
-
-
-# -----------------------------------------------------------------------------DRONY-------------------------------------------------------------
-def drones(request):
-    if request.user.is_authenticated:
-        drony = Drony.objects.get_queryset().order_by('-id_drona')
-        paginator = Paginator(drony, 5)
-        page_number = request.GET.get('page')
-        page_obj = paginator.get_page(page_number)
-        return render(request, 'rod/drones.html', {'title': 'Drony', 'drony': page_obj})
-
-    else:
-        return redirect('/')
+# -----------------------------------------------------------------------------TRASY-------------------------------------------------------------
 
 def routes(request):
     if request.user.is_authenticated:
@@ -91,6 +86,44 @@ def new_route(request):
                 trasa.save()
         return render(request, 'rod/new_route.html')
 
+def update_Route(request, pk):
+    if request.user.is_authenticated:
+        trasa = Trasy.objects.get(id_trasy = pk)
+        form = RouteUpdateForm(instance = trasa)
+
+        if request.method == 'POST':
+            form = RouteUpdateForm(request.POST, instance=trasa)
+            if form.is_valid():
+                form.save()
+                return redirect('/routes')
+        context = {'form':form}
+        return render(request, 'rod/route_update.html', context)
+    else:
+        return redirect('/')
+
+
+def delRoute(request, pk):
+    if request.user.is_authenticated:
+        trasa = Trasy.objects.get(id_trasy = pk)
+        if request.method == 'POST':
+            trasa.delete()
+            return redirect('/')
+        context = {'trasa': trasa}
+        return render(request, 'rod/route_confirm_delete.html', context)
+
+
+
+# -----------------------------------------------------------------------------DRONY-------------------------------------------------------------
+def drones(request):
+    if request.user.is_authenticated:
+        drony = Drony.objects.get_queryset().order_by('-id_drona')
+        paginator = Paginator(drony, 5)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        return render(request, 'rod/drones.html', {'title': 'Drony', 'drony': page_obj})
+
+    else:
+        return redirect('/')
 
 
 def new_drone(request):
@@ -106,20 +139,6 @@ def new_drone(request):
                 dron.save()
         return render(request, 'rod/new_drone.html', {'title': 'Drony_nowe'})
 
-def update_Route(request, pk):
-    if request.user.is_authenticated:
-        trasa = Trasy.objects.get(id_trasy = pk)
-        form = RouteUpdateForm(instance = trasa)
-
-        if request.method == 'POST':
-            form = RouteUpdateForm(request.POST, instance=trasa)
-            if form.is_valid():
-                form.save()
-                return redirect('/routes')
-        context = {'form':form}
-        return render(request, 'rod/route_update.html', context)
-    else:
-        return redirect('/')
 
 
 def updateDron(request, pk):
@@ -137,14 +156,6 @@ def updateDron(request, pk):
     else:
         return redirect('/')
 
-def delRoute(request, pk):
-    if request.user.is_authenticated:
-        trasa = Trasy.objects.get(id_trasy = pk)
-        if request.method == 'POST':
-            trasa.delete()
-            return redirect('/')
-        context = {'trasa': trasa}
-        return render(request, 'rod/route_confirm_delete.html', context)
 
 def delDron(request,pk):
     if request.user.is_authenticated:
@@ -157,16 +168,8 @@ def delDron(request,pk):
     else:
         return redirect('/')
 
-##-----------------------------------------------------------------------------DRONY-------------------------------------------------------------
-def map(request):
-    if request.user.is_authenticated:
-        zgloszenia = Zgloszenia.objects.all()
-        return render(request,'rod/map.html',{'title':'Mapa', 'zgloszenia':zgloszenia})
-    else:
-        return redirect('/')
 
-
-
+##-----------------------------------------------------------------------------ARCHIWUM-------------------------------------------------------------
 def all_archive(request):
     if request.user.is_authenticated:
         zgloszenia = Zgloszenia.objects.filter(zarejestrowane=True).order_by('-id_zgloszenia')
